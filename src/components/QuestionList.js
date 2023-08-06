ceeb51e1237f75feb1414ee1c8f7c423753e1b23
BreElle Wells <breellejordyn@gmail.com>
2023-08-06 13:05:26 -0600

    create new question using form

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
-  const [questions, setQuestions] = useState()
+  const [questions, setQuestions] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:4000/questions')
@@ -14,7 +14,7 @@ function QuestionList() {
     <section>
       <h1>Quiz Questions</h1>
       <ul>
-        {questions?.map((question) => (
+        {questions.map((question) => (
           <QuestionItem
             key={question.id}
             id={question.id}
